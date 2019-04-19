5ed0de6c380fc2465e0d60c798c0f0e6e85c1de6
ysf-aji <41295677+ysf-aji@users.noreply.github.com>
2019-04-19 21:32:22 +0800

    kategori produk.php

---
+++ application/views/kategori-produk.php
@@ -0,0 +1,100 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+<div class="grid-container daftar-kategori-produk">
+ 
+  <div class="grid-x kategori-produk kiri">
+    <div class="cell medium-6">
+      <img class="foto" src="<?php echo base_url(); ?>asset/img/kategori/tanaman-pangan.png" />
+    </div>
+    <div class="cell medium-6">
+      <div class="text">
+        <h3 class="">Pangan</h3>
+        <div class=""><a class="button primary" href="#">SELENGKAPNYA</a></div>
+      </div>
+    </div>
+  </div>
+  <div class="grid-x kategori-produk kanan">
+    <div class="cell medium-6">
+      <div class="text">
+        <h3 class="">Sayuran</h3>
+        <div class=""><a class="button primary" href="#">SELENGKAPNYA</a></div>
+      </div>
+    </div>
+    <div class="cell medium-6">
+      <img class="foto" src="<?php echo base_url(); ?>asset/img/kategori/sayur-sayuran.png" />
+    </div>
+  </div>
+  <div class="grid-x kategori-produk kiri">
+    <div class="cell medium-6">
+      <img class="foto" src="<?php echo base_url(); ?>asset/img/kategori/buah-buahan.png" />
+    </div>
+    <div class="cell medium-6">
+      <div class="text">
+        <h3 class="">Buah</h3>
+        <div class=""><a class="button primary" href="#">SELENGKAPNYA</a></div>
+      </div>
+    </div>
+  </div>
+  <div class="grid-x kategori-produk kanan">
+    <div class="cell medium-6">
+      <div class="text">
+        <h3 class="">Rimpang</h3>
+        <div class=""><a class="button primary" href="#">SELENGKAPNYA</a></div>
+      </div>
+    </div>
+    <div class="cell medium-6">
+      <img class="foto" src="<?php echo base_url(); ?>asset/img/kategori/rimpang.png" />
+    </div>
+  </div>
+ 
+ 
+ 
+</div><!-- .grid-container -->
