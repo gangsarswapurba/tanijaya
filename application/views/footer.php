6ccf1cf778b83fdc7ae6c3c51605f741d3eecad0
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2019-04-19 13:45:54 +0700

    add header & footer views

---
+++ application/views/footer.php
@@ -0,0 +1,53 @@
+<div class="grid-container full vertical-line">
+    <div class="grid-x">
+      <div class="cell medium-6 line">
+      </div>
+      <div class="cell medium-6">
+
+      </div>
+    </div>
+  </div>
+
+<div class="grid-container contact">
+    <div class="grid-x">
+      <div class="cell small-12">
+        <div class="lambang-tanya text-center"><ion-icon class="" name="ios-help-circle-outline"></ion-icon></div>
+      </div>
+    </div>
+    <div class="grid-x">
+      <div class="cell small-12">
+        <div class="tanya-langsung-text text-center">atau mau tanya-tanya langsung?</div>
+      </div>
+    </div>
+    <div class="grid-x">
+      <div class="cell small-12">
+        <div class="wa text-center"><a class="button primary" href="https://wa.me/6281904191601"><ion-icon name="logo-whatsapp"></ion-icon> <span>0819-0419-1601</span></a></div>
+      </div>
+    </div>
+  </div><!-- .contact -->
+
+  <div id="footer" class="grid-container full footer">
+      <div class="grid-x">
+        <div class="cell small-12 medium-4 text-center">
+          <span class="alamat">jln petung, ruko timur kfc papringan <br />(sebelah indonotebook dan roglasses)</span>
+        </div>
+        <div class="cell small-12 medium-4 text-center">
+          <a class="" href="."><img id="header-logo" src="<?php echo base_url(); ?>asset/img/logo.png" alt="" /></a>
+        </div>
+        <div class="cell small-12 medium-4 text-center">
+          <span class="media-sosial"><span>Ikuti kami</span> <a href="https://instagram.com"><ion-icon name="logo-instagram"></ion-icon></a></span>
+        </div>
+      </div>
+    </div><!-- .footer -->
+
+    <script src="https://unpkg.com/ionicons@4.5.5/dist/ionicons.js"></script>
+    <script src="<?php echo base_url(); ?>asset/js/vendor/jquery.js"></script>
+    <script src="<?php echo base_url(); ?>asset/js/vendor/what-input.js"></script>
+    <script src="<?php echo base_url(); ?>asset/js/vendor/foundation.js"></script>
+    <script src="<?php echo base_url(); ?>asset/js/app.js"></script>
+    <script>
+        $(document).foundation();
+    </script>
+</body>
+
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>TANIJAYA - The Best Sayuran Marketplace In The World</title>
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Mui:400:900|Playfair+Display:400,400i" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>asset/css/foundation.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>asset/css/app.css">
+</head>
+
+<body class="<?php echo $this->router->fetch_class(); ?>">
+
+    <div class="top-bar topbar-center-logo">
+        <div class="top-bar-left">
+          <ul class="menu">
+            <li>
+              <a id="search" href="#"><ion-icon name="ios-search"></ion-icon></a>
+            </li>
+          </ul>
+        </div>
+        <div class="top-bar-center">
+          <a class="" href="."><img id="header-logo" src="<?php echo base_url(); ?>asset/img/logo.png" alt="" /></a>
+        </div>
+        <div class="top-bar-right">
+            <ul class="menu">
+              <li>
+                <a id="cart" href="cart.html"><ion-icon name="cart"></ion-icon></a>
+              </li>
+            </ul>
+        </div>
+    </div>
