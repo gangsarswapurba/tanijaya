d005db29ea0ab21d8344cdf2d8630bd72909869b
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2019-04-22 23:20:46 +0700

    humanize cat prod name

--- application/views/kategori-produk.php
+++ application/views/kategori-produk.php
@@ -1,50 +1,40 @@
 <div class="grid-container daftar-kategori-produk">
 
+<?php $kiri = 0 ?>
+
+<?php foreach ($semua_kategori_produk as $key => $kategori_produk): ?>
+  <!-- <pre>
+  <?php //print_r($kategori_produk) ?>
+</pre> -->
+
+<?php $kiri == 1 ? $kiri = 0 : $kiri = 1;  ?>
+
+<?php if ( $kiri ): ?>
   <div class="grid-x kategori-produk kiri">
     <div class="cell medium-6">
-      <img class="foto" src="<?php echo base_url(); ?>asset/img/kategori/tanaman-pangan.png" />
+      <img class="foto" src="<?php echo $kategori_produk['foto']; ?>" />
     </div>
     <div class="cell medium-6">
       <div class="text">
-        <h3 class="">Pangan</h3>
+        <h3 class=""><?php echo humanize($kategori_produk['nama']); ?></h3>
         <div class=""><a class="button primary" href="#">SELENGKAPNYA</a></div>
       </div>
     </div>
   </div>
+<?php else: ?>
   <div class="grid-x kategori-produk kanan">
     <div class="cell medium-6">
       <div class="text">
-        <h3 class="">Sayuran</h3>
+        <h3 class=""><?php echo humanize($kategori_produk['nama']); ?></h3>
         <div class=""><a class="button primary" href="#">SELENGKAPNYA</a></div>
       </div>
     </div>
     <div class="cell medium-6">
-      <img class="foto" src="<?php echo base_url(); ?>asset/img/kategori/sayur-sayuran.png" />
+      <img class="foto" src="<?php echo $kategori_produk['foto']; ?>" />
     </div>
   </div>
-  <div class="grid-x kategori-produk kiri">
-    <div class="cell medium-6">
-      <img class="foto" src="<?php echo base_url(); ?>asset/img/kategori/buah-buahan.png" />
-    </div>
-    <div class="cell medium-6">
-      <div class="text">
-        <h3 class="">Buah</h3>
-        <div class=""><a class="button primary" href="#">SELENGKAPNYA</a></div>
-      </div>
-    </div>
-  </div>
-  <div class="grid-x kategori-produk kanan">
-    <div class="cell medium-6">
-      <div class="text">
-        <h3 class="">Rimpang</h3>
-        <div class=""><a class="button primary" href="#">SELENGKAPNYA</a></div>
-      </div>
-    </div>
-    <div class="cell medium-6">
-      <img class="foto" src="<?php echo base_url(); ?>asset/img/kategori/rimpang.png" />
-    </div>
-  </div>
+<?php endif; ?>
 
-
+<?php endforeach; ?>
 
 </div><!-- .grid-container -->
