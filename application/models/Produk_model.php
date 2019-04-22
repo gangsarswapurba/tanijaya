39c6e295d07567fad954a561a5fd85e662d8b4b4
Gunartatik16121023 <48718669+Gunartatik16121023@users.noreply.github.com>
2019-04-22 06:51:51 -0700

    Update Produk_model.php

--- application/models/Produk_model.php
+++ application/models/Produk_model.php
@@ -1,12 +1,19 @@
 <?php
-
+ 
 class Produk_model extends CI_Model {
-
+ 
+        public function get_top_5_product()
+        {
+                $this->db->limit(5);
+                $query = $this->db->get('produk');
+                return $query->result_array();
+        }
+ 
         public function get_product_detail($value)
         {
           $this->db->where('id', $value);
           $query = $this->db->get('produk');
           return $query->row_array();
         }
-
+ 
 }
