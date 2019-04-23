c1bba6e2223026fe384dc6a32e21f246375a4bcf
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2019-04-23 14:31:09 +0700

    focus input on search clicked

--- asset/js/app.js
+++ asset/js/app.js
@@ -3,6 +3,7 @@ $(document).foundation()
 // Open the full screen search box
 function openSearch() {
   document.getElementById("myOverlay").style.display = "block";
+  document.getElementById("kolom_pencarian").focus();
 }
 
 // Close the full screen search box
