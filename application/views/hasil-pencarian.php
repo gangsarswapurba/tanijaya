54d0a94bb1d9d403c032603204e7be941ab1303a
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2019-04-23 16:25:38 +0700

    add not found section in search

--- application/views/hasil-pencarian.php
+++ application/views/hasil-pencarian.php
@@ -3,16 +3,24 @@
   <h1>Hasil Pencarian: "<?php echo $yang_dicari; ?>"</h1>
 
   <div class="grid-x">
-    <?php foreach ($hasil_pencarian as $key => $produk): ?>
-      <a href="<?php echo base_url() ?>produk/index/<?php echo $produk['id'] ?>">
-      <div class="cell medium-3 produk-hasil-pencarian text-center">
-        <img src="<?php echo $produk['foto']; ?>" alt="<?php echo $produk['nama'] ?> <?php echo $produk['kota'] ?>">
-        <div class="kota"><?php echo $produk['kota']; ?></div>
-        <div class="nama"><?php echo $produk['nama']; ?></div>
-        <div class="harga">Rp<?php echo number_format($produk['harga']); ?>/<?php echo $produk['per'] == 1 ? '' : $produk['per']; ?><?php echo $produk['satuan'] ?></div>
+    <?php if (!empty($hasil_pencarian)): ?>
+      <?php foreach ($hasil_pencarian as $key => $produk): ?>
+        <a href="<?php echo base_url() ?>produk/index/<?php echo $produk['id'] ?>">
+        <div class="cell medium-3 produk-hasil-pencarian text-center">
+          <img src="<?php echo $produk['foto']; ?>" alt="<?php echo $produk['nama'] ?> <?php echo $produk['kota'] ?>">
+          <div class="kota"><?php echo $produk['kota']; ?></div>
+          <div class="nama"><?php echo $produk['nama']; ?></div>
+          <div class="harga">Rp<?php echo number_format($produk['harga']); ?>/<?php echo $produk['per'] == 1 ? '' : $produk['per']; ?><?php echo $produk['satuan'] ?></div>
+        </div>
+        </a>
+      <?php endforeach; ?>
+    <?php else: ?>
+      <div class="cell medium-12 text-center not-found">
+        <img src="<?php echo base_url(); ?>asset/img/iconfinder_Scare_3713833.png" alt="not found">
+        <h4>not found!</h4>
       </div>
-      </a>
-    <?php endforeach; ?>
+    <?php endif; ?>
+
     </div>
   </div>
 
