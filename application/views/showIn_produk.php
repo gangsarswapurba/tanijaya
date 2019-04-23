56e3c6fe312553ac545ad106373e64592cad69a9
Ardhi <ardhichoiruddin@yandex.com>
2019-04-23 20:57:29 +0700

    memperbaiki layout  produk di showIn_produk.php

--- application/views/showIn_produk.php
+++ application/views/showIn_produk.php
@@ -1,4 +1,5 @@
-
+<br>
+<br>
 <div class="fruit-container ">
     <div class="text-center">
         <h1>Buah</h1>
@@ -45,6 +46,41 @@
                     <p>Medium</p>
                     <p>Tomat</p>
                     <p>Rp. 10.000/kg</p>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <div class="d-flex show-produks">
+        <div class="m-1 m-l-r">
+            <a href="">
+                <img src="<?php echo base_url(); ?>asset/img/product/iconfinder_Strawberry_56028.png" alt="">
+                <div class="text-center">
+                    <p>Medium</p>
+                    <p>Strawbery</p>
+                    <p>Rp. 50.000/kg</p>
+                </div>
+            </a>
+        </div>
+
+        <div class="m-1 m-l-r">
+            <a href="">
+                <img src="<?php echo base_url(); ?>asset/img/product/iconfinder_Pear_56020.png" alt="">
+                <div class="text-center">
+                    <p>Medium</p>
+                    <p>Pear</p>
+                    <p>Rp. 100.000/kg</p>
+                </div>
+            </a>
+        </div>
+
+        <div class="m-1 m-l-r">
+            <a href="">
+                <img src="<?php echo base_url(); ?>asset/img/product/iconfinder_Kiwi_56021.png" alt="">
+                <div class="text-center">
+                    <p>Medium</p>
+                    <p>Kiwi</p>
+                    <p>Rp. 40.000/kg</p>
                 </div>
             </a>
         </div>
