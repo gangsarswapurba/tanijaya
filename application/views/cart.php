6003479b8492039dff79131350def056c3ff0200
mita16121043 <48484649+mita16121043@users.noreply.github.com>
2019-04-24 05:19:56 +0700

    memperbaiki layout cart.php
    agar bisa menampilkan cart keranjang belanja dan hitung total belanja

--- application/views/cart.php
+++ application/views/cart.php
@@ -16,3 +16,57 @@
     </div>
   </div>
 </div>
+<div class="grid-container cart">
+
+  <h2>Pesanan anda</h2>
+
+  <?php if (!empty($this->cart->contents())): ?>
+
+  <table cellpadding="6" cellspacing="1" style="width:100%" border="0">
+
+<?php $i = 1; ?>
+
+<?php foreach ($this->cart->contents() as $items): ?>
+
+        <?php echo form_hidden($i.'[rowid]', $items['rowid']); ?>
+
+        <tr>
+                <td class="foto"><a href="<?php echo base_url(); ?>produk/index/<?php echo $items['id']; ?>"><img src="<?php echo $items['foto'] ?>" alt=""></a></td>
+                <td class="nama">
+                        <?php echo $items['name'] . ' ' . $items['kota'] . ' (' . $items['per'] . $items['satuan'] . ')'; ?>
+
+                        <?php if ($this->cart->has_options($items['rowid']) == TRUE): ?>
+
+                                <p>
+                                        <?php foreach ($this->cart->product_options($items['rowid']) as $option_name => $option_value): ?>
+
+                                                <strong><?php echo $option_name; ?>:</strong> <?php echo $option_value; ?><br />
+
+                                        <?php endforeach; ?>
+                                </p>
+
+                        <?php endif; ?>
+
+                </td>
+                <td class="qty"><span class="qty-text">QTY:</span> <?php echo form_input(array('type' => 'number', 'name' => $i.'[qty]', 'value' => $items['qty'], 'maxlength' => '3', 'size' => '5')); ?></td>
+                <td class="keluarkan"><a href="<?php echo base_url(); ?>keranjang/keluarkan/<?php echo $items['rowid'] ?>"><ion-icon name="close-circle-outline"></ion-icon></a></td>
+        </tr>
+
+<?php $i++; ?>
+
+<?php endforeach; ?>
+
+</table>
+
+<div class="cell medium-12 text-center hitung-total">
+  <button class="button primary" type="button" name="button">HITUNG TOTAL</button>
+</div>
+
+  <?php else: ?>
+  <div class="cell medium-12 text-center not-found">
+    <img src="<?php echo base_url(); ?>asset/img/iconfinder_Scare_3713833.png" alt="not found">
+    <h4>anda belum memasukkan apa-apa</h4>
+</div>
+  <?php endif; ?>
+
+</div>
