3b0df94b8f3844b36a33dba61bc2603312f84ed1
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2019-04-27 04:13:23 +0700

    fix product category & search result layout

--- application/views/hasil-pencarian.php
+++ application/views/hasil-pencarian.php
@@ -5,14 +5,16 @@
   <div class="grid-x">
     <?php if (!empty($hasil_pencarian)): ?>
       <?php foreach ($hasil_pencarian as $key => $produk): ?>
-        <a href="<?php echo base_url() ?>produk/index/<?php echo $produk['id'] ?>">
-        <div class="cell medium-3 produk-hasil-pencarian text-center">
-          <img src="<?php echo base_url(); ?>assets/img/product/<?php echo $produk['foto']; ?>" alt="<?php echo $produk['nama'] ?> <?php echo $produk['kota'] ?>">
-          <div class="kota"><?php echo $produk['kota']; ?></div>
-          <div class="nama"><?php echo $produk['nama']; ?></div>
-          <div class="harga">Rp<?php echo number_format($produk['harga']); ?>/<?php echo $produk['per'] == 1 ? '' : $produk['per']; ?><?php echo $produk['satuan'] ?></div>
+        <div class="cell medium-3">
+          <a href="<?php echo base_url() ?>produk/index/<?php echo $produk['id'] ?>">
+          <div class="cell medium-3 produk-hasil-pencarian text-center">
+            <img src="<?php echo base_url(); ?>assets/img/product/<?php echo $produk['foto']; ?>" alt="<?php echo $produk['nama'] ?> <?php echo $produk['kota'] ?>">
+            <div class="kota"><?php echo $produk['kota']; ?></div>
+            <div class="nama"><?php echo $produk['nama']; ?></div>
+            <div class="harga">Rp<?php echo number_format($produk['harga']); ?>/<?php echo $produk['per'] == 1 ? '' : $produk['per']; ?><?php echo $produk['satuan'] ?></div>
+          </div>
+          </a>
         </div>
-        </a>
       <?php endforeach; ?>
     <?php else: ?>
       <div class="cell medium-12 text-center not-found">
