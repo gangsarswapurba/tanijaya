a7d361584371bf25cf389d3bd4bcfc753b69f7c3
Gunartatik <gesthiningtyas@gmail.com>
2019-06-22 05:57:47 -0700

    Menambah simbol mata uang Rupiah di sub total harga item di cart

--- application/views/cart.php
+++ application/views/cart.php
@@ -2,7 +2,7 @@
 
   <h2>Pesanan anda</h2>
 
-  <?php if (!empty($this->cart->contents())): ?>
+  <?php if($this->cart->contents()): ?>
 
   <table cellpadding="6" cellspacing="1" style="width:100%" border="0">
 
@@ -31,7 +31,7 @@
 
                 </td>
                 <td class="qty"><span class="qty-text">QTY:</span> <?php echo form_input(array('type' => 'number', 'name' => $i.'[qty]', 'value' => $items['qty'], 'maxlength' => '3', 'size' => '5')); ?></td>
-                <td><?php echo $this->cart->format_number($items['price']); ?></td>
+                <td>Rp<?php echo $this->cart->format_number($items['price']); ?></td>
                 <td class="keluarkan"><a href="<?php echo base_url(); ?>keranjang/keluarkan/<?php echo $items['rowid'] ?>"><ion-icon name="close-circle-outline"></ion-icon></a></td>
         </tr>
 
