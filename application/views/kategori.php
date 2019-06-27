e29cf5be8338cfec09540a06f6e3bc2b7edf507e
Ardhi <ardhichoiruddin@yandex.com>
2019-06-27 21:39:03 +0700

    mengganti dan mengubah ukuran font di display produk.

--- application/views/kategori.php
+++ application/views/kategori.php
@@ -5,13 +5,17 @@
   <div class="grid-x category-inner">
     <?php if (!empty($produk_produk)): ?>
       <?php foreach ($produk_produk as $key => $produk): ?>
-        <div class="cell medium-3">
+        <div class="cell medium-3 ">
           <a href="<?php echo base_url() ?>produk/index/<?php echo $produk['id'] ?>">
-          <div class="cell medium-3 produk-hasil-pencarian text-center">
-            <img src="<?php echo base_url(); ?>assets/img/product/<?php echo $produk['foto']; ?>" alt="<?php echo $produk['nama'] ?> <?php echo $produk['kota'] ?>">
-            <div class="kota"><?php echo $produk['kota']; ?></div>
-            <div class="nama"><?php echo $produk['nama']; ?></div>
-            <div class="harga">Rp<?php echo number_format($produk['harga']); ?>/<?php echo $produk['per'] == 1 ? '' : $produk['per']; ?><?php echo $produk['satuan'] ?></div>
+          <div class="cell medium-3 hovers produk-hasil-pencarian text-center">
+            <div class="c-produk">
+              <div class="cp-img">
+                <img src="<?php echo base_url(); ?>assets/img/product/<?php echo $produk['foto']; ?>" alt="<?php echo $produk['nama'] ?> <?php echo $produk['kota'] ?>">
+              </div>
+              <div class="kota"><?php echo $produk['kota']; ?></div>
+              <div class="nama"><?php echo $produk['nama']; ?></div>
+              <div class="harga">Rp<?php echo number_format($produk['harga']); ?>/<?php echo $produk['per'] == 1 ? '' : $produk['per']; ?><?php echo $produk['satuan'] ?></div>
+            </div>
           </div>
           </a>
         </div>
